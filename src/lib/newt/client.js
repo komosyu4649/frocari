3bd28e80d943f5d82fc32d7687fa469d7d260b5e
komosyu4649 <komosyu4649@gmail.com>
2023-01-02 00:44:46 +0900

    add: relation

--- src/lib/newt/client.js
+++ src/lib/newt/client.js
@@ -4,4 +4,7 @@ export const client = createClient({
   spaceUid: import.meta.env.PUBLIC_NEWT_SPACE_UID,
   token: import.meta.env.PUBLIC_NEWT_API_TOKEN,
   apiType: 'cdn',
+  // query: {
+  //   depth: 2,
+  // },
 })
