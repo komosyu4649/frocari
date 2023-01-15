1264452f4f10cfa1fda1a69a9a1a343b1341b968
komosyu4649 <komosyu4649@gmail.com>
2023-01-15 21:03:49 +0900

    wip: search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import image from '@astrojs/image';
 
 // https://astro.build/config
 
 // https://astro.build/config
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap'
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   // integrations: [image()]
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    sitemap(),
-  ],
-})
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), react()]
+});
